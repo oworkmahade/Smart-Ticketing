4778c33ba4becf0243cc4e6070295ea88c0fb682
oworkmahade <wtbl.hhasan@gmailcom>
2024-10-06 00:54:38 +0600

    commited

--- scripts/eTicket.js
+++ scripts/eTicket.js
@@ -3,6 +3,13 @@ let count = 0;
 let perSeatCost = 550.0;
 
 function seatBooked(event) {
+  const seat = event.target;
+  // conditional stopping execution
+  if (seat.classList.contains("bg-green")) {
+    window.alert("Seat already selected !");
+    return;
+  }
+
   const noOfSeat = seatIncrement(count);
   seatBookedDetails(event, noOfSeat);
   coupon(noOfSeat);
